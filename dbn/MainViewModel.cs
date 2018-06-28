0001fd38ede2757b55371d2c95993a04c8d86211
Amol Bhagwat <amolrbhagwat@gmail.com>
2018-06-28 18:18:57 -0400

    Reset the Results property to null on database connect
    Closes #11

--- dbn/MainViewModel.cs
+++ dbn/MainViewModel.cs
@@ -77,6 +77,7 @@ namespace dbn
             GenerateColumnTableMapping();
 
             Columns?.Clear();
+            Results = null;
         }
 
         private void GenerateColumnTableMapping()
